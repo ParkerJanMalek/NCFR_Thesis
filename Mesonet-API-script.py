97294f058b9db48d6d151d6e0f315d242a9b69d7
ParkerJanMalek <Parker_Malek@abtassoc.com>
2022-11-26 10:04:37 -0800

    update

--- Mesonet-API-script.py
+++ Mesonet-API-script.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 from datetime import datetime
+import scipy.io
 
 # Specify request parameters (as strings)
 token = 'aa31874e86fb42d9b2ea6b293f1bb004' # use your own token
@@ -16,12 +17,13 @@ def data_availability_check(station_name,token,var):
         return(True)
     else:
         return(False)
-
-station_name_list = ['ksfo','klax','krdd']#['C3BCC','C3BVS','C3CAT','C3DLA','C3DRW','C3FRC','C3GPO','C3HDC','C3HRD','C3NBB','C3NCM','C3POR','C3PVN','C3SKI','C3SKY','C3SOD','C3WDG','C3WPO'],
+#['ksfo','klax','krdd','ksmo','kcic','kpdx']'ksfo','klax','krdd','ksmo','kcic',#
+station_name_list = ['kpdx']#,'C3BCC','C3BVS','C3CAT','C3DLA','C3DRW','C3FRC','C3GPO','C3HDC','C3HRD','C3NBB','C3NCM','C3POR','C3PVN','C3SKI','C3SKY','C3SOD','C3WDG','C3WPO']
 
 # check for highest resolution of precip data 
 
 var = 'precip_accum_one_hour'
+unit = 'precip|mm'
 
 
 for i in station_name_list:
@@ -30,7 +32,7 @@ for i in station_name_list:
         'obtimezone':'UTC',
         'vars':var,
         'stids': station_name,
-        'units':'precip|mm',
+        'units':unit,
         'token':token
     }
 
@@ -53,7 +55,7 @@ for i in station_name_list:
             'vars': var,
             'stids': station_name,
                 #['C3BCC','C3BVS','C3CAT','C3DLA','C3DRW','C3FRC','C3GPO','C3HDC','C3HRD','C3NBB','C3NCM','C3POR','C3PVN','C3SKI','C3SKY','C3SOD','C3WDG','C3WPO'],
-            'units':'precip|mm',
+            'units':unit,
             'token':token
         }
         
@@ -72,39 +74,48 @@ for i in station_name_list:
         station_data_out = pd.Series(value_pull,index=pd.to_datetime(dateTime))
         
         # Retain only the hourly observations
-        #station_data_hourly = station_data_out.where(station_data_out.index.minute == 53).dropna()
+        station_data_hourly = station_data_out.resample('60min').mean()
+        #station_data_hourly = station_data_out.groupby(pd.Grouper(freq='60Min', offset=0, label='right')).first()
         
         # Plotting code
         fig,ax = plt.subplots(figsize=(20, 20))
-        plt.plot(station_data_out,linewidth=3)
+        plt.plot(station_data_hourly,linewidth=3)
         ax.set_ylabel(args['units'],fontsize=40)
         ax.tick_params(axis='both', which='major', labelsize=40)
         plt.xticks(rotation=90)
-        ax.set_title(args['stids'] + ' Hourly Precip (mm)',fontsize=40)
+        ax.set_title(args['stids'] + ' '+ args['units'],fontsize=40)
         # Clean up the plot a bit
         from matplotlib.dates import DayLocator, DateFormatter
         #ax.xaxis.set_major_locator(DayLocator())
         #ax.xaxis.set_major_formatter(DateFormatter('%d-%b'))
         ax.grid()
         
-        fig.savefig('temp2_'+station_name+'.png')
+        fig.savefig(args['vars'] + '_'+station_name+'.png')
+        
         
         #compute 95 percentile threshold
         
-        percentile_95 = station_data_out.quantile(0.95)
+        percentile_95 = station_data_hourly.quantile(0.95)
+        percentile_99 = station_data_hourly.quantile(0.99)
         
-        threshold_exceedance_points = station_data_out.loc[station_data_out >= percentile_95]
+        threshold_exceedance_points_95 = station_data_hourly.loc[station_data_hourly >= percentile_95]
+        threshold_exceedance_points_99 = station_data_hourly.loc[station_data_hourly >= percentile_99]
         
         
-        threshold_grouping= threshold_exceedance_points.groupby([threshold_exceedance_points.index.year, 
-                                                                 threshold_exceedance_points.index.month,
-                                                                 threshold_exceedance_points.index.day]).count()
+        threshold_grouping_95 = threshold_exceedance_points_95.groupby([threshold_exceedance_points_95.index.year, 
+                                                                 threshold_exceedance_points_95.index.month,
+                                                                 threshold_exceedance_points_95.index.day]).count()
+        
+        threshold_grouping_99 = threshold_exceedance_points_99.groupby([threshold_exceedance_points_99.index.year, 
+                                                                 threshold_exceedance_points_99.index.month,
+                                                                 threshold_exceedance_points_99.index.day]).count()
        
-        fig1,ax1 = plt.subplots(figsize=(300, 50))
-        threshold_grouping.plot(kind="bar")
+        fig1,ax1 = plt.subplots(figsize=(400, 50))
+        threshold_grouping_95.plot(kind="bar")
         ax1.set_ylabel('counts',fontsize=70)
         ax1.set_title('Precip occurrances for ' + station_name + ' where hourly rainfall rates exceed ' + str(percentile_95) + ' mm',fontsize=70)
         ax1.tick_params(axis='both', which='major', labelsize=40)
-        fig1.savefig('freq_over_exceedance_'+station_name+'.png')
+        fig1.savefig(args['vars'] + '_exceedance_'+station_name+'.png')
+        print('hi')
     else:
-        print("No data available")
+        print("No data available for " + station_name)
