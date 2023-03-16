f2b13061472d15fb4fbb3bb80671055f11c19318
ParkerJanMalek <Parker_Malek@abtassoc.com>
2023-03-15 23:35:04 -0700

    test grib import

--- MRMS_data_pull.py
+++ MRMS_data_pull.py
@@ -8,9 +8,10 @@ Pulls data based from MRMS database based on start and end dates
 """
 import datetime
 import wget
+import pygrib
 
-start_date = datetime.datetime(2020,1,1,1)
-end_date = datetime.datetime(2020,1,31,24)
+start_date = datetime.datetime(2020,1,1,0)
+end_date = datetime.datetime(2020,1,1,23)
 
 
 from datetime import date, timedelta
@@ -21,5 +22,5 @@ def daterange(start_date, end_date):
         
 for single_date in daterange(start_date, end_date):
     url = 'https://mtarchive.geol.iastate.edu/'+str(single_date.year)+'/'+str(single_date.month)+'/'+str(single_date.day)+'/mrms/ncep/'
-    print(url)
+   # gr = pygrib.open("MRMS_GaugeCorr_QPE_01H_00.00_20200202-000000.grib2")
         
