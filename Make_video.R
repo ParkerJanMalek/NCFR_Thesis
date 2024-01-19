0702119138b34f4cc4f3710f9a5bc3458aca8f68
Parker Malek <Parker_Malek@abtassoc.com>
2024-01-19 02:59:50 -0500

    additional updates

--- Make_video.R
+++ Make_video.R
@@ -12,49 +12,81 @@ dirs <- list.dirs("G:\\NCFR Thesis\\NCFR_Thesis\\")
 ivt_dirs <- grep("IVT_*", dirs, value = TRUE)
 radar_dirs <- grep("Radar_*", dirs, value = TRUE)
 combined_dirs <- grep("combined_*", dirs, value = TRUE)
-for (j in ivt_dirs){
-  regex_folder <- rev(mixedsort(list.files(j,"^IVT.*\\.png$")))
+# for (j in ivt_dirs){
+#   regex_folder <- rev(mixedsort(list.files(j,"^IVT.*\\.png$")))
+# 
+#   #for(i in regex_folder){
+#     # Get a list of image file names in the directory
+#     image_files <- paste0(j,"\\",regex_folder)#list.files(paste0(j,"\\",i), pattern = "QPE", full.names = TRUE)
+#    
+#      # Create a video writer
+#     output_file <- paste0("KOVE_IVT_Pulse.gif")
+#     
+#     av::av_encode_video(image_files, paste0(j,"\\",output_file), framerate = 6)
+#   
+#  # }
+# }
+# 
+# for (j in ivt_dirs){
+#   regex_folder <- rev(mixedsort(list.files(j,"^850.*\\.png$")))
+#   
+#   #for(i in regex_folder){
+#   # Get a list of image file names in the directory
+#   image_files <- paste0(j,"\\",regex_folder)#list.files(paste0(j,"\\",i), pattern = "QPE", full.names = TRUE)
+#   
+#   # Create a video writer
+#   output_file <- paste0("KOVE_Tadv_Pulse.gif")
+#   
+#   av::av_encode_video(image_files, paste0(j,"\\",output_file), framerate = 6)
+#   
+#   # }
+# }
+# 
+# for (j in radar_dirs){
+#   regex_folder <- mixedsort(list.files(j,"^KBBX.*\\.png$"))
+#   
+#   #for(i in regex_folder){
+#   # Get a list of image file names in the directory
+#   image_files <- paste0(j,"\\",regex_folder)#list.files(paste0(j,"\\",i), pattern = "QPE", full.names = TRUE)
+#   
+#   # Create a video writer
+#   output_file <- paste0("KOVE_Radar_Pulse.gif")
+#   
+#   av::av_encode_video(image_files, paste0(j,"\\",output_file), framerate = 6)
+#   
+#   # }
+# }
 
-  #for(i in regex_folder){
-    # Get a list of image file names in the directory
-    image_files <- paste0(j,"\\",regex_folder)#list.files(paste0(j,"\\",i), pattern = "QPE", full.names = TRUE)
-   
-     # Create a video writer
-    output_file <- paste0("KOVE_IVT_Pulse.gif")
-    
-    av::av_encode_video(image_files, paste0(j,"\\",output_file), framerate = 6)
-  
- # }
-}
-
-for (j in ivt_dirs){
-  regex_folder <- rev(mixedsort(list.files(j,"^850.*\\.png$")))
+#IVT combined
+for (j in combined_dirs){
+  regex_folder <- mixedsort(list.files(j,"^IVT.*\\.png$"))
   
   #for(i in regex_folder){
   # Get a list of image file names in the directory
   image_files <- paste0(j,"\\",regex_folder)#list.files(paste0(j,"\\",i), pattern = "QPE", full.names = TRUE)
   
   # Create a video writer
-  output_file <- paste0("KOVE_Tadv_Pulse.gif")
+  output_file <- paste0("KOVE_combined_IVT_Pulse.mov")
   
   av::av_encode_video(image_files, paste0(j,"\\",output_file), framerate = 6)
   
   # }
 }
-
-for (j in radar_dirs){
-  regex_folder <- mixedsort(list.files(j,"^KBBX.*\\.png$"))
+#Tadv combined
+for (j in combined_dirs){
+  regex_folder <- mixedsort(list.files(j,"^850.*\\.png$"))
   
   #for(i in regex_folder){
   # Get a list of image file names in the directory
   image_files <- paste0(j,"\\",regex_folder)#list.files(paste0(j,"\\",i), pattern = "QPE", full.names = TRUE)
   
   # Create a video writer
-  output_file <- paste0("KOVE_Radar_Pulse.gif")
+  output_file <- paste0("KOVE_combined_Tadv_Pulse.mov")
   
   av::av_encode_video(image_files, paste0(j,"\\",output_file), framerate = 6)
   
   # }
 }
+
 
 #utils::browseURL(output_file)
